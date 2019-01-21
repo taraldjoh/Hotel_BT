b7860d2af6a6e9ff72a863dafb2a0e623d16a748
taraldjoh <tarald.johansen@outlook.com>
2019-01-21 20:44:05 +0100

    Tiny edits
    Edited some colors
    Added some hover effects

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-window.OnScroll = function() {stkNav()};
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {
+//   myFunction();
+// };
 
-var navbar = document.getElementById("navbar");
+// // Get the navbar
+// var navbar = document.getElementById("navbar");
 
-var sticky = navbar.offsetTop(0);
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
 
-function stkNav() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky")
-    }
-}
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky");
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
